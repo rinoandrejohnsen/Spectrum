e20f828fd9866c464e4bdb4641e226d90b6cba20
Rino André Johnsen <rinoandrejohnsen@gmail.com>
2012-11-20 12:02:05 +0100

    Uncommented some stuff

--- public_html/source/modules/shapeModule/shapeModule.js
+++ public_html/source/modules/shapeModule/shapeModule.js
@@ -3,17 +3,9 @@ define(['source/common/infrastructure/model'], function(model) {
     //shapeModule.prototype = new model(['PHP', 'JavaScript']);
 
     function shapeModule(shapeRegion) {
-        //debugger;
         try {
             curl(['wire!source/modules/shapeModule/shapeModule.spec'], function(context) {
                 context.wire({
-//                    shelli: {
-//                            render: {
-//                            template: {module: 'text!source/modules/shapeModule/resources/templates/circleTemplate.html'},
-//                            css: {module: 'css!source/modules/shapeModule/resources/styles/circleStyle.css'}
-//                        },
-//                        insert: {at: 'dom.first!body'}  
-//                    },
                     plugins: [
                         {
                             module: 'wire/debug', 
